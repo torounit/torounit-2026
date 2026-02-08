e9b582fd1e8108b6a8a3bf2004e82f7535f4c10f
torounit <mail@torounit.com>
2026-02-08 22:37:45 +0900

    feat: fix oembed post

--- functions.php
+++ functions.php
@@ -2,3 +2,14 @@
 add_action( 'enqueue_block_assets', function () {
 	wp_enqueue_style( 'google-fonts', 'https://fonts.googleapis.com/css2?family=Zen+Kaku+Gothic+New:wght@300;400;500;700;900&display=swap', array(), null );
 } );
+
+add_action( 'after_setup_theme', function () {
+	$GLOBALS['content_width'] = apply_filters( 'content_width', 856 );
+} );
+
+add_filter( 'oembed_min_max_width', function () {
+	return array(
+		'min' => 200,
+		'max' => 856,
+	);
+} );
