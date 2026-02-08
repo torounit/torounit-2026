1bae44f13c9c8eebbde4ce41b31493a9904c3b26
torounit <mail@torounit.com>
2026-02-08 20:06:22 +0900

    feat: fix header

--- functions.php
+++ functions.php
@@ -1,4 +1,4 @@
 <?php
-add_action( 'wp_enqueue_scripts', function () {
+add_action( 'enqueue_block_assets', function () {
 	wp_enqueue_style( 'google-fonts', 'https://fonts.googleapis.com/css2?family=Zen+Kaku+Gothic+New:wght@300;400;500;700;900&display=swap', array(), null );
 } );
