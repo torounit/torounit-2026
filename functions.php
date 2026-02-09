8e39a4c2c91938ef2dfff8c482547be887d87152
torounit <mail@torounit.com>
2026-02-09 12:55:40 +0900

    feat: post meta data

--- functions.php
+++ functions.php
@@ -1,6 +1,7 @@
 <?php
 add_action( 'enqueue_block_assets', function () {
 	wp_enqueue_style( 'google-fonts', 'https://fonts.googleapis.com/css2?family=Zen+Kaku+Gothic+New:wght@300;400;500;700;900&display=swap', array(), null );
+	wp_enqueue_style( 'material-symbols', 'https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200', array(), null );
 } );
 
 add_action( 'after_setup_theme', function () {
