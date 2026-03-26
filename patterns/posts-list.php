28b5fddcf52d7666fb4fe382adde05105c5e0f38
torounit <mail@torounit.com>
2026-03-26 20:14:48 +0900

    fix: add links

--- patterns/posts-list.php
+++ patterns/posts-list.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Title: List of posts
  * Slug: torounit-2026/posts-list
@@ -9,11 +10,12 @@
  */
 ?>
 
-<!-- wp:query {"query":{"perPage":3,"pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":true},"metadata":{"categories":["posts"],"patternName":"torounit-2026/posts-list","name":"List of Posts"}} -->
+<!-- wp:query {"query":{"perPage":10,"pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":true},"metadata":{"categories":["posts","posts"],"patternName":"torounit-2026/posts-list","name":"List of Posts"}} -->
 <div class="wp-block-query"><!-- wp:post-template -->
     <!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
-    <div class="wp-block-group"><!-- wp:post-title {"level":3} /-->
-        <!-- wp:post-date {"metadata":{"bindings":{"datetime":{"source":"core/post-data","args":{"field":"date"}}}},"style":{"elements":{"link":{"color":{"text":"var:preset|color|dim"}}}},"textColor":"dim"} /-->
+    <div class="wp-block-group"><!-- wp:post-title {"level":3,"isLink":true} /-->
+
+        <!-- wp:post-date {"isLink":true,"metadata":{"bindings":{"datetime":{"source":"core/post-data","args":{"field":"date"}}}},"style":{"elements":{"link":{"color":{"text":"var:preset|color|dim"}}}},"textColor":"dim"} /-->
     </div>
     <!-- /wp:group -->
     <!-- /wp:post-template -->
